742fa8c218eef3f99295c523c0c4d09ef179563d
shiv <shiv@singsys.com>
2023-10-05 17:27:57 +0530

    add personal information

---
+++ whoIAm/introduction.php
@@ -0,0 +1,3 @@
+<?php 
+	echo "I am Shiv Om Mishra.I am from Badshah Nagar Barabanki UP.I am currently working as a Software Engineer at Singsys Pte Ltd Lucknow.We have completed my B.Tech with Computer Science and Engineering in 2022. We have completed my Diploma with Information Technology in 2019.";
+ ?>
